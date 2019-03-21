ec3c3fadb4bf8d27ed4781f00d937b285d8a0405
jpclark6 <jpclark6@gmail.com>
2019-03-21 15:46:16 -0600

    Adds test to check for favorites Co-authered-by: Jsmith23 <jaystanmith@gmail.com> Co-authered-by: Jpclark6 <jpclark6@gmail.com>

--- queries/connection.js
+++ queries/connection.js
@@ -1,4 +1,6 @@
 const knexfile = require("../knexfile");
-const knex = require("knex")(knexfile[process.env.NODE_ENV]);
+// const knex = require("knex")(knexfile[process.env.NODE_ENV]);
+const environment = process.env.NODE_ENV || 'development'; 
+const knex = require("knex")(knexfile[environment]);
 
 module.exports = knex;
